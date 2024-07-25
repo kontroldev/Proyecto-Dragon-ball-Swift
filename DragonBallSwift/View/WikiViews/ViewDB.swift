6375bc617f0556e30da052353baad6324a7a1e13
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2024-07-25 18:30:24 -0400

    Cambia estilos de la Saga Dragon Ball para adaptarse al diseno de figma

--- DragonBallSwift/View/WikiViews/ViewDB.swift
+++ DragonBallSwift/View/WikiViews/ViewDB.swift
@@ -9,36 +9,71 @@ import SwiftUI
 
 struct ViewDB: View {
     @State private var viewModel: AllCharactersDBViewModel = AllCharactersDBViewModel()
-    
     @State private var isLoadig = false
     
+    let columns = [GridItem(), GridItem()]
+    
     var body: some View {
-        
-        NavigationStack{
-            ZStack{
-                LinearGradient(colors: [.white, .purple], startPoint: .topLeading, endPoint: .bottomTrailing)
-                    .ignoresSafeArea()
-                List{
-                    ForEach(viewModel.AllCharacters, id: \.id){item in
-                        VStack(alignment: .leading){
-                            HStack{
-                                AsyncImage(url: URL(string: item.image)){ image in
-                                    image.resizable()
-                                }placeholder: {
-                                    ProgressView()
-                                }.frame(width: 100, height: 100)
-                                Text(item.name).font(.title2).bold()
+        NavigationStack {
+            VStack {
+//                Text("Personajes")
+//                    .font(.title)
+//                    .foregroundStyle(.accent)
+//                    .fontWeight(.bold)
+                
+                ScrollView {
+                    LazyVGrid(columns: columns, spacing: 20) {
+                        ForEach(viewModel.AllCharacters, id: \.id) { item in
+                            ZStack {
+                                VStack {
+                                    AsyncImage(url: URL(string: item.image)){ image in
+                                        image
+                                            .resizable()
+                                            .scaledToFit()
+                                            .offset(x: 60, y: 30)
+                                    } placeholder: {
+                                        ProgressView()
+                                    }
+                                }
+                                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomLeading)
+                                .scaleEffect(2)
                                 
-                                
+                                VStack {
+                                    Text(item.name)
+                                        .font(.title3)
+                                        .bold()
+                                        .foregroundStyle(.accent)
+                                        .padding(8)
+                                        .padding(.top, 10)
+                                        .multilineTextAlignment(.leading)
+                                        .frame(maxWidth: 120, maxHeight: .infinity, alignment: .topLeading)
+                                }
+                                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
                             }
-                            Text(item.planet)
+                            .frame(height: 120)
+                            .background(Color("CardColor"))
+                            .clipShape(RoundedRectangle(cornerRadius: 12))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 12)
+                                    .stroke(Color.gray.opacity(0.2), lineWidth: 1)
+                            )
+                            .padding(.horizontal, 4)
                         }
                     }
-                    .listRowBackground(Color.clear)
                 }
-                .listStyle(PlainListStyle())
-                
-                .navigationTitle("Dragon Ball")
+            }
+            .navigationTitle("Personajes")
+            .navigationBarTitleDisplayMode(.inline)
+            .padding(.horizontal, 4)
+            .background(Color("BackgroundColor"))
+            .toolbar {
+                ToolbarItem {
+                    Button {
+                        //TODO: Implementar funcionalidad de buscar personaje
+                    } label: {
+                        Image(systemName: "magnifyingglass")
+                    }
+                }
             }
         }
     }
