14c3c8bf8ab4470ef9a01b79e7924a4247a0aabd
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2024-07-25 21:16:37 -0400

    Crea componente básico de personaje para ser reutilizable en otras vistas

--- DragonBallSwift/View/WikiViews/ViewDB.swift
+++ DragonBallSwift/View/WikiViews/ViewDB.swift
@@ -16,48 +16,10 @@ struct ViewDB: View {
     var body: some View {
         NavigationStack {
             VStack {
-//                Text("Personajes")
-//                    .font(.title)
-//                    .foregroundStyle(.accent)
-//                    .fontWeight(.bold)
-                
                 ScrollView {
                     LazyVGrid(columns: columns, spacing: 20) {
                         ForEach(viewModel.AllCharacters, id: \.id) { item in
-                            ZStack {
-                                VStack {
-                                    AsyncImage(url: URL(string: item.image)){ image in
-                                        image
-                                            .resizable()
-                                            .scaledToFit()
-                                            .offset(x: 60, y: 30)
-                                    } placeholder: {
-                                        ProgressView()
-                                    }
-                                }
-                                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomLeading)
-                                .scaleEffect(2)
-                                
-                                VStack {
-                                    Text(item.name)
-                                        .font(.title3)
-                                        .bold()
-                                        .foregroundStyle(.accent)
-                                        .padding(8)
-                                        .padding(.top, 10)
-                                        .multilineTextAlignment(.leading)
-                                        .frame(maxWidth: 120, maxHeight: .infinity, alignment: .topLeading)
-                                }
-                                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
-                            }
-                            .frame(height: 120)
-                            .background(Color("CardColor"))
-                            .clipShape(RoundedRectangle(cornerRadius: 12))
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 12)
-                                    .stroke(Color.gray.opacity(0.2), lineWidth: 1)
-                            )
-                            .padding(.horizontal, 4)
+                            BasicCharacterCardView(character: item)
                         }
                     }
                 }
