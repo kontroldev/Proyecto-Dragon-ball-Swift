67f46fb8424cd20e3459c2920f1ab9f669757b80
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2024-07-25 21:28:49 -0400

    Se adapta la interfaz de los dragones al diseno de Figma

--- DragonBallSwift/View/WikiViews/ViewDragons.swift
+++ DragonBallSwift/View/WikiViews/ViewDragons.swift
@@ -12,31 +12,64 @@ struct ViewDragons: View {
     @State private var viewModel: AllDragonsViewModel = AllDragonsViewModel()
     @State private var isLoadig = false
     
+    let columns = [GridItem(), GridItem()]
+    
     var body: some View {
-        NavigationStack{
-            ZStack{
-                LinearGradient(colors: [.white, .green], startPoint: .topLeading, endPoint: .bottomTrailing)
-                    .ignoresSafeArea()
-                List{
-                    ForEach(viewModel.AllDragos, id: \.id){item in
-                        VStack(alignment: .leading){
-                            HStack{
-                                AsyncImage(url: URL(string: item.image)){ image in
-                                    image.resizable()
-                                }placeholder: {
-                                    ProgressView()
-                                }.frame(width: 100, height: 100)
-                                Text(item.name).font(.title2).bold()
+        NavigationStack {
+            VStack {
+                ScrollView {
+                    LazyVGrid(columns: columns, spacing: 20) {
+                        ForEach(viewModel.AllDragos, id: \.id) { character in
+                            ZStack {
+                                VStack {
+                                    AsyncImage(url: URL(string: character.image)) { image in
+                                        image
+                                            .resizable()
+                                            .scaledToFit()
+                                            .offset(x: 70, y: 20)
+                                    } placeholder: {
+                                        ProgressView()
+                                    }
+                                }
+                                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomLeading)
+                                .scaleEffect(1.5)
                                 
-                                
+                                VStack {
+                                    Text(character.name)
+                                        .font(.title3)
+                                        .bold()
+                                        .foregroundStyle(.accent)
+                                        .padding(8)
+                                        .padding(.top, 10)
+                                        .multilineTextAlignment(.leading)
+                                        .frame(maxWidth: 120, maxHeight: .infinity, alignment: .topLeading)
+                                }
+                                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
                             }
-                            
+                            .frame(height: 120)
+                            .background(Color("CardColor"))
+                            .clipShape(RoundedRectangle(cornerRadius: 12))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 12)
+                                    .stroke(Color.gray.opacity(0.2), lineWidth: 1)
+                            )
+                            .padding(.horizontal, 4)
                         }
                     }
-                    .listRowBackground(Color.clear)
                 }
-                .listStyle(PlainListStyle())
-                .navigationTitle("Dragones")
+            }
+            .navigationTitle("Personajes")
+            .navigationBarTitleDisplayMode(.inline)
+            .padding(.horizontal, 4)
+            .background(Color("BackgroundColor"))
+            .toolbar {
+                ToolbarItem {
+                    Button {
+                        //TODO: Implementar funcionalidad de buscar personaje
+                    } label: {
+                        Image(systemName: "magnifyingglass")
+                    }
+                }
             }
         }
     }
