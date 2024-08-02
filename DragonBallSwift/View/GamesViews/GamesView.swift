309ad85496f0f20f3e850c2e10050264cfb8aeb0
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2024-08-02 14:32:57 -0400

    Mejora la UI de navegación para memory game, implementando un fullScreenCover

--- DragonBallSwift/View/GamesViews/GamesView.swift
+++ DragonBallSwift/View/GamesViews/GamesView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct GamesView: View {
     
+    @State private var isAnElementTapped: Bool = false
+    @State private var selectedGame: String = ""
+    
     let menuItems = [
-        ItemMenu(name: "Memory game", imegenName: "logoDBGM", destination: AnyView(MemoryGameView())),
+        ItemMenu(name: "Memory game", imegenName: "logoDBGM", destination: AnyView(MemoryGameView(dismiss: .constant(false)))),
         ItemMenu(name: "Tetrix", imegenName: "GokuTetrix", destination: AnyView(HomeTreixView()))
     ]
     
@@ -24,28 +27,35 @@ struct GamesView: View {
                 
                 ScrollView {
                     ForEach(menuItems) { item in
-                        NavigationLink(destination: item.destination) {
-                            HStack {
-                                Text(item.name).font(.title2).bold()
-                                    .foregroundStyle(.accent)
-                                
-                                Spacer()
-                                
-                                Image(item.imegenName)
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(height: 150)
-                            }
-                            .padding()
-                            .background(Color("CardColor"))
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 12)
-                                    .stroke(Color.gray.opacity(0.2), lineWidth: 1)
-                            )
-                            .cornerRadius(12)
+                        HStack {
+                            Text(item.name).font(.title2).bold()
+                                .foregroundStyle(.accent)
+                            
+                            Spacer()
+                            
+                            Image(item.imegenName)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 150)
                         }
+                        .padding()
+                        .background(Color("CardColor"))
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 12)
+                                .stroke(Color.gray.opacity(0.2), lineWidth: 1)
+                        )
+                        .cornerRadius(12)
                         .padding(.horizontal)
                         .padding(.top, 8)
+                        .onTapGesture {
+                            isAnElementTapped = true
+                            selectedGame = item.name
+                        }
+                        .fullScreenCover(isPresented: $isAnElementTapped) {
+                            if selectedGame == "Memory game" {
+                                MemoryGameView(dismiss: $isAnElementTapped)
+                            }
+                        }
                     }
                 }
             }
